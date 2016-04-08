ffd9456d6049587792accbe0afeb7da90143fedd
theory friction practice <jackson.brebnor@gmail.com>
2016-04-08 12:12:36 -0400

    changes gemspec to reflect my own fork

--- lib/rubycards/card.rb
+++ lib/rubycards/card.rb
@@ -44,14 +44,14 @@ module RubyCards
     # @return [String] The string (of glyph) representation of the card's suit
     def suit(glyph = false)
       case @suit
-        when 1
-          glyph ? CLUB.black.bold : 'Clubs'
-        when 2
-          glyph ? DIAMOND.red : 'Diamonds'
-        when 3
-          glyph ? HEART.red : 'Hearts'
-        when 4
-          glyph ? SPADE.black.bold : 'Spades'
+      when 1
+        glyph ? CLUB.black.bold : 'Clubs'
+      when 2
+        glyph ? DIAMOND.red : 'Diamonds'
+      when 3
+        glyph ? HEART.red : 'Hearts'
+      when 4
+        glyph ? SPADE.black.bold : 'Spades'
       end
     end
 
@@ -86,36 +86,36 @@ module RubyCards
       # A simple template with X's as placeholders
       # YY represents the placement of the card's rank
       template = <<-TPL.gsub(/^\s+/,'')
-        ╭───────╮
-        | X X X |
-        | X X X |
-        | X YYX |
-        | X X X |
-        ╰───────╯
+      ╭───────╮
+      | X X X |
+      | X X X |
+      | X YYX |
+      | X X X |
+      ╰───────╯
       TPL
 
       # the patterns represent the configuration of glyphys
       #   read from left to right, top to bottom
       # X means place a glyph, _ means clear the space
       case @rank
-        when 2;  pattern = '_X_______X_'
-        when 3;  pattern = '_X__X____X_'
-        when 4;  pattern = 'X_X_____X_X'
-        when 5;  pattern = 'X_X_X___X_X'
-        when 6;  pattern = 'X_XX_X__X_X'
-        when 7;  pattern = 'X_X_X_XXX_X'
-        when 8;  pattern = 'X_XX_XXXX_X'
-        when 9;  pattern = 'X_XXXXXXX_X'
-        when 10; pattern = 'XXXX_XXXXXX'
-        when 11..14;
-          pattern = 'X_________X'
+      when 2;  pattern = '_X_______X_'
+      when 3;  pattern = '_X__X____X_'
+      when 4;  pattern = 'X_X_____X_X'
+      when 5;  pattern = 'X_X_X___X_X'
+      when 6;  pattern = 'X_XX_X__X_X'
+      when 7;  pattern = 'X_X_X_XXX_X'
+      when 8;  pattern = 'X_XX_XXXX_X'
+      when 9;  pattern = 'X_XXXXXXX_X'
+      when 10; pattern = 'XXXX_XXXXXX'
+      when 11..14;
+        pattern = 'X_________X'
       end
 
       pattern.each_char do |c|
         # replace X's with glyphs
         if c == 'X'
           template.sub!(/X/, "#{suit(true)}")
-        # replace _'s with whitespace
+          # replace _'s with whitespace
         else
           template.sub!(/X/, " ")
         end
@@ -137,7 +137,7 @@ module RubyCards
     # @return [Integer] An integer representation of the rank (ordered)
     def rank_to_i(rank)
       case rank.to_s
-        when /^(a|ace)/i;   14
+      when /^(a|ace)/i;   14
         when /^(k|king)/i;  13
         when /^(q|queen)/i; 12
         when /^(j|jack)/i;  11
@@ -155,10 +155,10 @@ module RubyCards
     def suit_to_i(suit)
       case suit
         when /^club/i;    1
-        when /^diamond/i; 2
-        when /^heart/i;   3
-        when /^spade/i;   4
-        else 0
+      when /^diamond/i; 2
+      when /^heart/i;   3
+      when /^spade/i;   4
+      else 0
       end
     end
 
--- rubycards.gemspec
+++ rubycards.gemspec
@@ -4,14 +4,14 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'rubycards/version'
 
 Gem::Specification.new do |gem|
-  gem.name          = "rubycards"
+  gem.name          = 'rubyrummy'
   gem.version       = RubyCards::VERSION
-  gem.authors       = ["Jordan Scales", "Joe Letizia", "Justin Workman"]
-  gem.email         = ["scalesjordan@gmail.com", "joe.letizia@gmail.com", "xtagon@gmail.com"]
-  gem.description   = "RubyCards"
-  gem.licenses      = ["MIT"]
-  gem.summary       = "A 52 card gem"
-  gem.homepage      = "https://github.com/jdan/rubycards"
+  gem.authors       = ['theory friction practice', 'Jordan Scales', 'Joe Letizia', 'Justin Workman']
+  gem.email         = ['jackson.brebnor@gmail.com', 'scalesjordan@gmail.com', 'joe.letizia@gmail.com', 'xtagon@gmail.com']
+  gem.description   = 'Rummy game based on RubyCards'
+  gem.licenses      = ['MIT']
+  gem.summary       = 'An implementation of Rummy card game gem'
+  gem.homepage      = 'https://github.com/tfpractice/rubyrummy'
 
   gem.files         = `git ls-files`.split($/)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
