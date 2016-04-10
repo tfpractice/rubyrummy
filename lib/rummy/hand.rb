ca58f72af237ac107000fefe288daba4478116c8
theory friction practice <jackson.brebnor@gmail.com>
2016-04-09 21:58:18 -0400

    tests Hand#select_card, #select_rank, #select_suit

--- lib/rummy/hand.rb
+++ lib/rummy/hand.rb
@@ -2,15 +2,29 @@ require_relative 'card'
 
 module Rummy
   class Hand < RubyCards::Hand
-    def_delegators :cards, :size
-    # def initialize(args)
-    #
-    # end
-    def discard(deck, card)
-      n.times do
-        @cards << deck.draw unless deck.empty?
-      end
-      # self
+    def_delegators :cards, :size,
+      # def initialize(args)
+      #
+      # end
+      #def discard(deck, card)
+      #  n.times do
+      #    @cards << deck.draw unless deck.empty?
+      #  end
+      #  # self
+      #end
+    def discard(card)
+      rejected_card =  cards.delete(card)
+    end
+    def select_card(rank, suit)
+      cards.select { |c| c.rank == rank && c.suit == suit }.first
+    end
+    def select_by_suit(suit)
+      cards.select { |c| c.suit == suit }
+
+    end
+    def select_by_rank(rank)
+      cards.select { |c| c.rank == rank }
+
     end
 
   end
--- spec/rummy/hand_spec.rb
+++ spec/rummy/hand_spec.rb
@@ -2,6 +2,8 @@ include Rummy
 
 describe Hand do
   let(:myHand) { Hand.new }
+  let(:specDeck) { MainDeck.new }
+
   it 'inherits from RubyCards::Hand' do
     expect(Hand.superclass).to eq(RubyCards::Hand)
   end
@@ -10,4 +12,45 @@ describe Hand do
       expect(myHand).to respond_to(:size)
     end
   end
+  context 'when hand has cards' do
+    before(:each) do
+      specDeck.shuffle!
+      myHand.draw(specDeck, 10)
+    end
+    describe '#select_card' do
+      it 'returns the card specified by rank and suit' do
+        r0 = myHand[0].rank
+        s0 = myHand[0].suit
+        expect(myHand.select_card(r0,s0)).to be_a_kind_of(Card)
+      end
+    end
+    describe '#select_by_rank' do
+      it 'returns all the cards in hand of the matching rank' do
+        r0 = myHand[0].rank
+        puts myHand.select_by_rank(r0)
+        expect(myHand.select_by_rank(r0)).to be_a_kind_of(Array)
+      end
+    end
+    describe '#select_by_suit' do
+      it 'returns all the cards in hand of the matching rank' do
+        s0 = myHand[0].suit
+        puts myHand.select_by_suit(s0)
+        expect(myHand.select_by_suit(s0)).to be_a_kind_of(Array)
+      end
+    end
+    describe '#discard' do
+      it 'reduces cards by 1' do
+        puts myHand
+        c1 = myHand[1]
+        myHand.discard(c1)
+        puts myHand
+        expect(myHand).not_to include(c1)
+
+
+      end
+    end
+  end
+
+  #
+  # end
 end
