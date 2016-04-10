599b83ef923dfcc44a67a2540238d50e2680e704
theory friction practice <jackson.brebnor@gmail.com>
2016-04-10 16:41:13 -0400

    tests Rummy::Hand#discard

--- lib/rummy/hand.rb
+++ lib/rummy/hand.rb
@@ -15,16 +15,17 @@ module Rummy
     def discard(card)
       rejected_card =  cards.delete(card)
     end
+
     def select_card(rank, suit)
       cards.select { |c| c.rank == rank && c.suit == suit }.first
     end
+
     def select_by_suit(suit)
       cards.select { |c| c.suit == suit }
-
     end
+
     def select_by_rank(rank)
       cards.select { |c| c.rank == rank }
-
     end
 
   end
