f2ccff6bbbdefe02a4b60630a71d7392e5635ca8
theory friction practice <jackson.brebnor@gmail.com>
2016-04-09 22:01:16 -0400

    corrects syntas in Rummy::Hand delegator

--- lib/rummy/hand.rb
+++ lib/rummy/hand.rb
@@ -2,16 +2,16 @@ require_relative 'card'
 
 module Rummy
   class Hand < RubyCards::Hand
-    def_delegators :cards, :size,
-      # def initialize(args)
-      #
-      # end
-      #def discard(deck, card)
-      #  n.times do
-      #    @cards << deck.draw unless deck.empty?
-      #  end
-      #  # self
-      #end
+    def_delegators :cards, :size
+    # def initialize(args)
+    #
+    # end
+    #def discard(deck, card)
+    #  n.times do
+    #    @cards << deck.draw unless deck.empty?
+    #  end
+    #  # self
+    #end
     def discard(card)
       rejected_card =  cards.delete(card)
     end
