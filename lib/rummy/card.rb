e3d0737d0ce565be260467a10acb510fcfd86a18
theory friction practice <jackson.brebnor@gmail.com>
2016-04-12 13:04:56 -0400

    abstracts rank precedence using Rummy::Deck::Rank constants

--- lib/rummy/card.rb
+++ lib/rummy/card.rb
@@ -5,18 +5,19 @@ module Rummy
     # end
 
     def pred
-      pred_id = (rank_id - 1) % Rummy::Deck::RANKS.length
+      pred_id = (rank_id - 1) %  ranks_length
       pred_rank = Rummy::Deck::RANKS[pred_id]
     end
 
     def succ
-      succ_id = (rank_id + 1) % Rummy::Deck::RANKS.length
+      succ_id = (rank_id + 1) % ranks_length
       succ_rank = Rummy::Deck::RANKS[succ_id]
     end
     def rank_id
       Rummy::Deck::RANKS.index(rank)
     end
-
-
+    def ranks_length
+      Rummy::Deck::RANKS.length
+    end
   end
 end
