7401ccc6da21869311417e03026eba1b50d07daa
theory friction practice <jackson.brebnor@gmail.com>
2016-04-12 13:02:38 -0400

    tests palydeck#plays array

--- lib/rummy/play_deck.rb
+++ lib/rummy/play_deck.rb
@@ -2,9 +2,15 @@ require_relative 'deck'
 
 module Rummy
   class PlayDeck < Deck
+    attr_accessor :plays
     def initialize(player)
       @player = player
+      @plays = []
       super
+    end
+
+    def add_play(newPlay)
+      plays << newPlay
     end
 
 
--- spec/rummy/play_deck_spec.rb
+++ spec/rummy/play_deck_spec.rb
@@ -1,6 +1,15 @@
 include Rummy
 describe PlayDeck do
+  let(:p1) { Player.new(name: 'p1') }
+  let(:p2) { Player.new(name: 'p2') }
+  let!(:myGame) { Game.new(players: [p1, p2]) }
+  let(:myDeck) { p1.play_deck }
   it 'inherits from Rummy::Deck' do
     expect(PlayDeck.superclass).to eq(Rummy::Deck)
+  end
+  describe 'initialize' do
+    it 'starts with an empty @plays array' do
+      expect(myDeck.plays).to be_a_kind_of(Array)
+    end
   end
 end
