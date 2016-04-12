d22c09a1a8a51b346b9e10ba1f17b1c061ca0093
theory friction practice <jackson.brebnor@gmail.com>
2016-04-12 12:59:53 -0400

    tests rankplay initialization

--- lib/rummy/rank_play.rb
+++ lib/rummy/rank_play.rb
@@ -1,9 +1,18 @@
 module Rummy
   class RankPlay < RubyCards::Hand
-    # def initialize(args)
-    #
-    # end
+    def initialize(cards = [])
+      raise "too few cards: #{cards.length}" unless min_count?(cards)
+      raise "cards must be of same rank" unless shared_rank?(cards)
+      super
+    end
+    def min_count?(cards)
+      cards.length >= 3
+    end
 
+    def shared_rank?(cards)
+      first_rank = cards[0].rank
+      cards.all? { |c| c.rank == first_rank  }
 
+    end
   end
 end
--- spec/rummy/rank_play_spec.rb
+++ spec/rummy/rank_play_spec.rb
@@ -1,6 +1,26 @@
 include Rummy
 describe RankPlay do
+  let(:my3s) { Card.new(3, 'Spades') }
+  let(:my3h) { Card.new(3, 'Hearts') }
+  let(:my3d) { Card.new(3, 'Diamonds') }
+  let(:my4s) { Card.new(4, 'Spades') }
+  let(:myPlay) { RankPlay.new([my3s, my3h, my3d]) }
   it 'inherits from RubyCards::Hand' do
     expect(RankPlay.superclass).to eq(RubyCards::Hand)
+  end
+  describe '#initialize' do
+    context 'when given threee cards of equal rank' do
+      it 'returns a new play' do
+        expect(myPlay).to be_a_kind_of(RankPlay)
+      end
+    end
+    context 'when given three cards of different rank' do
+      it 'raises a RuntimeError' do
+        expect { RankPlay.new([my3s, my3h, my4s]) } .to raise_error(RuntimeError)
+      end
+    end
+  end
+  it 'has three cards' do
+    expect(myPlay.cards.length).to eq(3)
   end
 end
