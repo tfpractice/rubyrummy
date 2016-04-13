c683fc4a6f29b6441fc448e2044033c1b72d3063
theory friction practice <jackson.brebnor@gmail.com>
2016-04-12 21:17:28 -0400

    properly tests SuitPlay#consecutive

--- lib/rummy/suit_play.rb
+++ lib/rummy/suit_play.rb
@@ -1,9 +1,24 @@
 module Rummy
   class SuitPlay < RubyCards::Hand
-    # def initialize(args)
+    def initialize(cards = [])
+      raise "too few cards: #{cards.length}" unless min_count?(cards)
+      raise "cards must be of same suit" unless shared_suit?(cards)
+      raise "cards must be consecutive" unless consecutive?(cards)
+      super
+    end
 
-    # end
 
+    def min_count?(cards)
+      cards.length >= 3
+    end
 
+    def shared_suit?(cards)
+      first_suit = cards[0].suit
+      cards.all? { |c| c.suit == first_suit }
+    end
+
+    def consecutive?(cards)
+      cards.sort.each_cons(2).all? { |x, y| y.rank == x.succ_rank.to_s }
+    end
   end
 end
--- spec/rummy/suit_play_spec.rb
+++ spec/rummy/suit_play_spec.rb
@@ -1,6 +1,39 @@
 include Rummy
 describe SuitPlay do
+  let(:my3s) { Card.new(3, 'Spades') }
+  let(:my4s) { Card.new(4, 'Spades') }
+  let(:my5s) { Card.new(5, 'Spades') }
+  let(:my4d) { Card.new(4, 'Diamonds') }
+
+
+  let(:myPlay) { SuitPlay.new([my3s, my4s, my5s]) }
+
+  # let(:my3s) { Card.new(3, 'Spades') }
+  let(:my3h) { Card.new(3, 'Hearts') }
+  let(:my3d) { Card.new(3, 'Diamonds') }
+  # let(:my4s) { Card.new(4, 'Spades') }
+  # let(:myPlay) { RankPlay.new([my3s, my3h, my3d]) }
   it 'inherits from RubyCards::Hand' do
     expect(SuitPlay.superclass).to eq(RubyCards::Hand)
+  end
+  describe '#initialize' do
+    context 'when given threee cards of equal suit' do
+      it 'returns a new play' do
+        expect(myPlay).to be_a_kind_of(SuitPlay)
+      end
+    end
+    context 'when given three cards of different suit' do
+      it 'raises a RuntimeError' do
+        expect { SuitPlay.new([my3s, my3h, my4s]) } .to raise_error(RuntimeError)
+      end
+    end
+  end
+  describe '#consecutive?' do
+    it 'checks if all the cards are consecutive' do
+      expect(myPlay.consecutive?(myPlay.cards)).to eq(true)
+    end
+  end
+  it 'has three cards' do
+    expect(myPlay.cards.length).to eq(3)
   end
 end
